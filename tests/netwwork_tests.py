Fix: Network modification and error backpropagation rule testing on the network.

tests/netwwork_tests.py
@@ -1,5 +1,8 @@
 import unittest
 
+import torch
+from scipy.constants import troy_ounce
+
 
 class MyTestCase(unittest.TestCase):
     def test_supernet_input_output(self):
@@ -28,6 +31,8 @@ class MyTestCase(unittest.TestCase):
     def test_backbone_to_superres(self):
         import torch
         from tinynetwork.supreres import SuperResolution
+        import torch.nn as nn
+        import torch.optim as optim
 
         l2_yolo_back_bone = torch.rand(1, 64, 160, 160)
         l4_yolo_back_bone = torch.rand(1, 128, 80, 80)
@@ -36,6 +41,28 @@ class MyTestCase(unittest.TestCase):
         superNet = SuperResolution(num_channel=3, c1=64, c2=128, scale_factor=2)
         output_res = superNet(l4_yolo_back_bone, l2_yolo_back_bone)
         print(output_res.shape)
+
+        criterion = nn.MSELoss()
+        optimizer = optim.SGD(superNet.parameters(), lr=0.01)
+        target = torch.rand(1, 3, 1280, 1280)
+
+        loss = criterion(output_res, target)
+        optimizer.zero_grad()
+        loss.backward()
+        for name, param in superNet.named_parameters():
+            if param.grad is None:
+                print("\U0001F468", f"No gradient for {name}")
+                return False
+            if torch.isnan(param.grad).any():
+                print("\U0001F648", f"NaN in gradient of {name}")
+                return False
+            if torch.all(param.grad == 0):
+                print(f"\U0001F468", f"All-zero gradient for {name}")
+
+                return False
+            print(f"U+1F648  Gradient OK for {name}")
+        return True
+
 
     def test_load_and_getinfo_yolov12n(self):
         from ultralytics import YOLO
@@ -46,7 +73,8 @@ class MyTestCase(unittest.TestCase):
         # x = torch.rand(1, 3, 640, 640)
         # out = model(x)
         # print(out)
-        print(model)
+        print(model.model.model[21].cv2[-1][-1])
+        # print(model)
         # print(f"All parameter: {count_parameters(model)}")
         #
         # print(f"trainable parameters: {count_trainable_parameters(model)}")
@@ -95,32 +123,11 @@ class MyTestCase(unittest.TestCase):
         from tinynetwork.yoloo import SuperYoloo
         import torch
 
-        model = SuperYoloo(yolo_model_src="/home/my_proj/weights/pretrained_models/yolo12n.pt")
+        model = SuperYoloo()
+        print(model)
+        import sys; sys.exit()
         input = torch.rand(1, 3, 640, 640)
         output, _ = model(input)
-
-    def test_get_output_from_yolo_head(self):
-        from ultralytics import YOLO
-        import torch
-        import torch.nn as nn
-        from tinynetwork.yoloo import Concat
-        yolo_model = YOLO("/home/my_proj/weights/pretrained_models/yolo12n.pt")
-
-        yolo_model = yolo_model.model.model
-        # neck = full_model[9:21]
-        neck = nn.Sequential(yolo_model[9], Concat(), yolo_model[11], yolo_model[12], yolo_model[13], yolo_model[14],
-                                  yolo_model[15], yolo_model[15], yolo_model[16], yolo_model[17], yolo_model[18], yolo_model[19],
-                                  yolo_model[20])
-        dummy_input = torch.rand(1, 256, 20, 20)
-
-        x = dummy_input
-
-        for i, layer in enumerate(neck):
-            try:
-                x = layer(x)
-                print(f"Layer {i} output shape: {x.shape}")
-            except Exception as error:
-                print(f"error: {error}")
 
 
     def test_custome_yolo(self):
@@ -132,6 +139,7 @@ class MyTestCase(unittest.TestCase):
 
         p3, p4, p5 = backbone_net(input)
         print(p5)
+
     def test_custome_yolo_head(self):
         from tinynetwork.yoloo import BackBone, Head
         import torch
@@ -142,6 +150,145 @@ class MyTestCase(unittest.TestCase):
         p3, p4, p5 = backbone_net(input)
         d = head_net(p3, p4, p5)
         print(d)
+
+    def test_new_super_yolo_model(self):
+        from tinynetwork.yoloo import SuperYoloo, SuperResolution
+        from utils.util import count_parameters, count_trainable_parameters, count_layer_wise_parameters
+        my_net = SuperYoloo()
+        input = torch.rand(1, 3, 640, 640)
+        x, y = my_net(input)
+
+    def test_new_super_yolo_model(self):
+        from tinynetwork.yoloo import SuperYoloo, SuperResolution
+        from utils.util import count_parameters, count_trainable_parameters, count_layer_wise_parameters
+
+        my_net = SuperYoloo()
+        input = torch.rand(1, 3, 640, 640)
+        x, y = my_net(input)
+        print(f"number of paprameters: {count_parameters(my_net)}")
+        print(f"number of trainable parameters: {count_trainable_parameters(my_net)}")
+        print(type(x))
+
+    def test_backpropagation_on_my_network(self):
+        from tinynetwork.yoloo import SuperYoloo
+        import torch
+        import torch.nn as nn
+        import torch.optim as optim
+
+        my_net = SuperYoloo()
+
+        my_net.train()
+        criterion = nn.L1Loss()
+        criterion2 = nn.MSELoss()
+        optimizer = optim.SGD(my_net.parameters(), lr=0.01)
+        input = torch.rand(1, 3, 640, 640)
+        target_super = torch.rand(1, 3, 1280, 1280)
+        target_yolo1 = torch.rand(1, 144, 80, 80)
+        target_yolo2 = torch.rand(1, 144, 40, 40)
+        target_yolo3 = torch.rand(1, 144, 20, 20)
+
+
+        output, output_super = my_net(input)
+        loss1 = criterion2(output[0], target_yolo1)
+        loss2 = criterion2(output[1], target_yolo2)
+        loss3 = criterion2(output[2], target_yolo3)
+        loss4 = criterion(output_super, target_super)
+        total_loss = 4 * loss1 + 2 * loss2 + loss3 + 1.5 * loss4
+        optimizer.zero_grad()
+        total_loss.backward()
+
+        for name, param in my_net.named_parameters():
+            if param.grad is None:
+                print(f"[❌] No gradient for {name}")
+                return False
+            if torch.isnan(param.grad).any():
+                print(f"[❌] NaN in gradient of {name}")
+                return False
+            if torch.all(param.grad == 0):
+                print(f"[❌] All-zero gradient for {name}")
+                return False
+            print(f"[✅] Gradient OK for {name}")
+        return True
+
+    def test_infrence(self):
+        from tinynetwork.yoloo import SuperYoloo
+        import torch
+        import torch.nn as nn
+        import torch.optim as optim
+
+        my_net = SuperYoloo(tr_model=False)
+        input = torch.rand(1, 3, 640, 640)
+        my_net.eval()
+        with torch.no_grad():
+            output = my_net(input)
+            print(output)
+
+    def test_save_model(self):
+        from tinynetwork.yoloo import SuperYoloo
+        import torch
+        import torch.nn as nn
+        import torch.optim as optim
+        from torch.utils.data import DataLoader, Dataset
+
+        class MyDataSet(Dataset):
+            def __init__(self):
+                self.data = torch.rand(50, 3, 640, 640)
+                self.lable1 = torch.rand(50, 144, 80, 80)
+                self.lable2 = torch.rand(50, 144, 40, 40)
+                self.lable3 = torch.rand(50, 144, 20, 20)
+                self.label4 = torch.rand(50, 3, 1280, 1280)
+            def __len__(self):
+                return self.data.shape[0]
+
+            def __getitem__(self, item):
+                return self.data[item], self.lable1[item], self.lable2[item], self.lable3[item], self.label4[item]
+
+
+        my_dataset = MyDataSet()
+        train_set = DataLoader(dataset=my_dataset, batch_size=1, shuffle=True)
+        my_net = SuperYoloo(tr_model=True).to('cuda')
+
+        criterion = nn.L1Loss()
+        criterion2 = nn.MSELoss()
+
+        optimizer = optim.SGD(my_net.parameters(), lr=0.01)
+        for epoch in enumerate(range(5)):
+            my_net.train()
+            train_loss = 0
+            for data, lb1, lb2, lb3, lb4 in train_set:
+                r_data = data.to('cuda')
+                lb1 = lb1.to('cuda')
+                lb2 = lb2.to('cuda')
+                lb3 = lb3.to('cuda')
+                lb4 = lb4.to('cuda')
+                output0, output1 = my_net(r_data)
+                loss1 = criterion2(output0[0], lb1)
+                loss2 = criterion2(output0[1], lb2)
+                loss3 = criterion2(output0[2], lb3)
+                loss4 = criterion(output1, lb4)
+                total_loss = loss1 + loss2 + loss3 + loss4
+                optimizer.zero_grad()
+                total_loss.backward()
+                optimizer.step()
+
+            checkpoint = {
+                'epoch': epoch,
+                'model_state_dict': my_net.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': total_loss,
+                # Add any other relevant info
+                }
+
+            torch.save(checkpoint, 'checkpoint.pth')
+
+        # checkpoint = torch.load('checkpoint.pth')
+        # model.load_state_dict(checkpoint['model_state_dict'])
+        # optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        # epoch = checkpoint['epoch']
+        # loss = checkpoint['loss']
+        #
+        # model.train()  # Set back to training mode
+        # # Continue training from saved epoch
 
 if __name__ == '__main__':
     unittest.main()

tinynetwork/supreres.py
@@ -66,7 +66,7 @@ class Decoder(nn.Module):
         high_level_feature = self.conv2(high_level_feature)
         high_level_feature = self.relu(high_level_feature)
         high_level_feature = F.interpolate(high_level_feature, size=[i*(scaler_factor//2) for i in low_level_feature.size()[2:]], mode='bilinear', align_corners=True)
-        if scaler_factor>1:
+        if scaler_factor > 1:
             low_level_feature = F.interpolate(low_level_feature, size=[i*(scaler_factor//2) for i in low_level_feature.size()[2:]], mode='bilinear', align_corners=True)
         high_level_feature = torch.cat((high_level_feature, low_level_feature), dim=1)
         high_level_feature = self.last_conv(high_level_feature)

tinynetwork/yoloo.py
@@ -1,37 +1,13 @@
-import sys
 import torch
 import torch.nn as nn
 from torch import Tensor
 from tinynetwork.supreres import SuperResolution
 from ultralytics import YOLO
-from typing import List
-import torch.functional as F
+from utils.util import active_trainable_params
+from typing import Tuple
 
-YOLO_MODEL = YOLO("yolo12n.pt").model.model
-
-
-class Concat(nn.Module):
-    # Concatenate a list of tensors along dimension
-    def __init__(self, dimension=1):
-        super(Concat, self).__init__()
-        self.d = dimension
-
-    def forward(self, x):
-        try:
-            out = torch.cat(x, self.d)
-            return out
-        except Exception as error:
-            print(f"error '{error}'")
-            print(f"tensor1: {x[0].shape} | tensor2: {x[-1].shape}")
-            b0, c0, w0, h0 = x[0].shape
-            b1, c1, w1, h1 = x[-1].shape
-            if w0 == w1 and h0 != h1:
-                pad = torch.rand(b0, c0, w0, h0 - h1)
-                x[-1] = torch.cat((x[-1], pad), dim=3)
-            elif w0 != w1 and h0 != h1:
-                x[-1] = F.interpolate(x[-1], size=(w0, h0), mode='bilinear', align_corners=False)
-            out = torch.cat(x, self.d)
-            return out
+YOLO_MODEL = YOLO("/home/my_proj/weights/pretrained_models/yolo12n.pt").model.model
+active_trainable_params(YOLO_MODEL)
 
 class BackBone(nn.Module):
     def __init__(self):
@@ -45,6 +21,7 @@ class BackBone(nn.Module):
         self.l6 = YOLO_MODEL[6]
         self.l7 = YOLO_MODEL[7]
         self.l8 = YOLO_MODEL[8]
+
     def forward(self, x):
         p1 = self.l0(x)
         p2 = self.l1(p1)
@@ -55,7 +32,7 @@ class BackBone(nn.Module):
         p4 = self.l6(p4)
         p5 = self.l7(p4)
         p5 = self.l8(p5)
-        return p3, p4, p5
+        return p2, p3, p4, p5
 
 class Head(nn.Module):
     def __init__(self):
@@ -73,6 +50,37 @@ class Head(nn.Module):
         self.l19 = YOLO_MODEL[19]
         self.l20 = YOLO_MODEL[20]
         self.l21 = YOLO_MODEL[21]
+        for idx, module in enumerate(self.l21.cv3):
+            for jdx, block in enumerate(module):
+                if jdx == 2:
+                    block.out_channels = 1
+        # for idx, module in enumerate(self.l21.cv3):
+        #     for jdx, block in enumerate(module):
+        #         if jdx == 2:
+        #             block.in_channels = 18
+        #             block.out_channels = 18
+        #             break
+        #         for kdx, layers in enumerate(block):
+        #             if idx == 0 and jdx == 0 and kdx == 0:
+        #                 continue
+        #             elif idx == 0 and jdx == 0 and kdx == 1:
+        #                 layers.conv.out_channels = 18
+        #                 layers.bn.num_features = 18
+        #             else:
+        #                 layers.conv.in_channels = 18
+        #                 layers.conv.out_channels = 18
+        #                 layers.bn.num_features = 18
+
+
+        # for item in self.l21.cv3:
+        #     for idx, layer in enumerate(item):
+        #         if idx == 2:
+        #             break
+        #         for idx, l in enumerate(layer):
+        #             if idx == 1:
+        #                 l.conv.out_channels = 1
+        #                 l.bn.num_features=1
+
     def forward(self, p3, p4, p5):
         u1 = self.l9(p5)
         c1 = self.l10((u1, p4))
@@ -89,28 +97,21 @@ class Head(nn.Module):
         d = self.l21([h2, h4, h6])
         return d
 
-
-
 class SuperYoloo(nn.Module):
-    def __init__(self, yolo_model_src: str) -> None:
+    def __init__(self, tr_model: bool=True):
         super().__init__()
-        yolo_model = YOLO(yolo_model_src).model.model
-        self.backbone = yolo_model[:9]
-        self.neck = yolo_model[9:21]
-        self.head = yolo_model[21]
-        self.super_res = SuperResolution(num_channel=3, c1=64, c2=256, scale_factor=2)
-        self.storage_layer_output: List[Tensor] = []
+        self.tr_mode = tr_model
+        self.back_bone = BackBone()
+        self.super = SuperResolution(num_channel=3, c1=64, c2=128, scale_factor=2)
+        self.head = Head()
 
-    def forward(self, x: Tensor) -> Tensor:
-        for i, layer in enumerate(self.backbone):
-            try:
-                x = layer(x)
-                if i == 2 or i == 7 or i == 8:
-                    self.storage_layer_output.append(x)
-            except Exception as error:
-                print(i, f"error: {error}")
-        output_res = self.super_res(self.storage_layer_output[1], self.storage_layer_output[0])
-        output_neck = self.neck(self.storage_layer_output[-1])
-        output = self.head(output_neck)
-        return self.storage_layer_output, output
-
+    def forward(self, input: Tensor) -> Tuple[Tensor, Tensor]:
+        if self.tr_mode:
+            p2, p3, p4, p5 = self.back_bone(input)
+            output = self.head(p3, p4, p5)
+            super_output = self.super(p3, p2)
+            return output, super_output
+        else:
+            p2, p3, p4, p5 = self.back_bone(input)
+            output = self.head(p3, p4, p5)
+            return output